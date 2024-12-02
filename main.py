339b9eb20d7f4d9b18f2b7a16c48447e2541438a
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-01 20:26:47 -0500

    Doc: Created documentation for the integrate_linear_function() function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from scipy.integrate import quad
 
 def integrate_linear_function(a, b, start, end):
+
+    """Integrates a linear function f(x) = ax + b over a specified range.
+
+    Parameters:
+        a (float): Coefficient of x.
+        b (float): Constant term.
+        start (float): Start of the integration range.
+        end (float): End of the integration range.
+
+    Returns:
+        float: The definite integral result.
+    """
     f = lambda x: a * x + b
     result, _ = quad(f, start, end)
     return result
